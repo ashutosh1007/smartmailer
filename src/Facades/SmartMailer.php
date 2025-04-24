a0f3520590b3a8f4016e1af702608f6c55d6e08f
Ashutosh <lbi.ashutosh@gmail.com>
2025-04-24 20:24:48 +0530

    chore: added phpdoc block and annotations

--- src/Facades/SmartMailer.php
+++ src/Facades/SmartMailer.php
@@ -4,8 +4,23 @@ namespace SmartMailer\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Provides static access to the SmartMailer service.
+ *
+ * @method static \SmartMailer\SmartMailer to(string|array $recipient)
+ * @method static \SmartMailer\SmartMailer type(string $type)
+ * @method static bool send(\Illuminate\Mail\Mailable|\SmartMailer\SmartMailable $mailable)
+ * @method static bool sendMailable(\Illuminate\Mail\Mailable|\SmartMailer\SmartMailable $mailable)
+ *
+ * @see \SmartMailer\SmartMailer
+ */
 class SmartMailer extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'smartmailer';
